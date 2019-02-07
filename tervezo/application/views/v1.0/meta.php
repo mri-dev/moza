715651c71b3f3feae34b6259db6bbba14442b643
mri-dev <pocokxtrame@gmail.com>
2019-02-07 21:01:58 +0100

    Save

---
+++ tervezo/application/views/v1.0/meta.php
@@ -0,0 +1,27 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- STYLES -->
+<link rel="icon" href="<?=IMG?>icons/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+<?=$this->addStyle('bootstrap.min', 'media="all"')?>
+<?=$this->addStyle('bootstrap-theme.min', 'media="all"')?>
+<?=$this->addStyle('FontAwesome.min', 'media="all"')?>
+<?=$this->addStyle('dashicons.min','media="all"')?>
+<!--<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">-->
+<?=$this->addStyle('media', 'media="all"', false)?>
+<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/angular_material/1.1.4/angular-material.min.css" />
+
+<!-- JS's -->
+<!-- Angular Material requires Angular.js Libraries -->
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-aria.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-messages.min.js"></script>
+<?=$this->addJS('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',true)?>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+<script src='//www.google.com/recaptcha/api.js?hl=hu'></script>
+
+<!-- Angular Material Library -->
+<script src="//ajax.googleapis.com/ajax/libs/angular_material/1.1.4/angular-material.min.js"></script>
+<?=$this->addJS('bootstrap.min',false)?>
+<?=$this->addJS('app',false,false)?>
